7db0e0f4da429cfefcc2b01d71888e6a0549609f
aartiarh <164339816+aartiarh@users.noreply.github.com>
2024-03-23 15:13:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +17,21 @@ Hello World
                              \____\_______/
 """
     return '<pre>' + docker_whale + '</pre>'
+
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+@app.route('/prime/<int:n>')
+def prime(n):
+    """Return a list of prime numbers up to n."""
+    primes = [str(i) for i in range(2, n+1) if is_prime(i)]
+    return "Primes up to " + str(n) + ": " + ", ".join(primes)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
